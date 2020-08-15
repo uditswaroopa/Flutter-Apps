0ffb88770f72cf06091b18c4f99baf3c9896dc63
uditswaroopa <uditswaroopa@icloud.com>
2020-08-15 11:35:52 +0530

    http request

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:quotes/pages/loading.dart';
 import 'package:quotes/pages/location.dart';
 void main() => runApp(
   MaterialApp(
-  initialRoute: '/home',
+  initialRoute: '/',
   routes: {
     '/' : (context) => Loading(),
     '/home' : (context) => Home(),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +8,23 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  
+  void getData() async{
+    Response r = await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(r.body);
+    print(data);
+    print(data['title']);
+  }
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Text('Loading Page'),
+        body: Text('Loadin Page'),
     );
   }
 }
